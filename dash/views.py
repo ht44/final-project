336aae5eec8846e8e692c2798679c2f6f8599e2d
Hayden Turek <haydenturek@gmail.com>
2017-07-10 21:04:09 -0500

    passed tests for dash idx resp attributes

--- dash/views.py
+++ dash/views.py
@@ -3,5 +3,15 @@ from django.http import HttpResponse
 import json
 
 def index(request):
-    res = {'level': 'sector', 'year': 2015}
+    res = {
+        'level': 'sector',
+        'year': 2015,
+        'use_matrix': '',
+        'make_matrix': '',
+        'industry_vector': '',
+        'commodity_vector': '',
+        'value_vector': '',
+        'demand_vector': '',
+        'noncomp_vector': '',
+    }
     return HttpResponse(json.dumps(res), content_type='application/json')

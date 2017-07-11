0c7e6806c28394caada732439703ea893b084816
Hayden Turek <haydenturek@gmail.com>
2017-07-10 20:35:50 -0500

    wrote tests for dash index view

--- dash/tests.py
+++ dash/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
+
+client = Client()
+
+class DashApiTest(TestCase):
+    def test_dash_index_resp_sector_mostrec_year(self):
+        response = client.get('/dash/')
+        parsed = response.json()
+        self.assertEqual(parsed.level, 'sector')
+        self.assertEqual(parsed.year, '2015')
 
 # Create your tests here.

197a201c962ef82d684a59e839565dbc0ce39443
Hayden Turek <haydenturek@gmail.com>
2017-07-10 22:06:41 -0500

    wrote tests for dash api view filter

--- dash/tests.py
+++ dash/tests.py
@@ -5,7 +5,7 @@ import numpy as np
 
 client = Client()
 
-class DashApiTest(TestCase):
+class DashApiIndexTest(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.econ = m.Leontief('sector', '2015', sql=True)
@@ -21,6 +21,8 @@ class DashApiTest(TestCase):
     def test_dash_index_resp(self):
         self.assertEqual(self.parsed['level'], 'sector')
         self.assertEqual(self.parsed['year'], 2015)
+
+    def test_dash_index_resp_attr(self):
         self.assertIn('use_matrix', self.parsed)
         self.assertIn('make_matrix', self.parsed)
         self.assertIn('industry_vector', self.parsed)
@@ -28,6 +30,8 @@ class DashApiTest(TestCase):
         self.assertIn('value_vector', self.parsed)
         self.assertIn('demand_vector', self.parsed)
         self.assertIn('noncomp_vector', self.parsed)
+
+    def test_dash_index_resp_data(self):
         self.assertEqual(self.parsed['use_matrix'], self.econ.use_matrix.tolist())
         self.assertEqual(self.parsed['make_matrix'], self.econ.make_matrix.tolist())
         self.assertEqual(self.parsed['industry_vector'], self.econ.industry_vector.tolist())
@@ -35,3 +39,24 @@ class DashApiTest(TestCase):
         self.assertEqual(self.parsed['value_vector'], self.econ.value_vector.tolist())
         self.assertEqual(self.parsed['demand_vector'], self.econ.demand_vector.tolist())
         self.assertEqual(self.parsed['noncomp_vector'], self.econ.noncomp_vector.tolist())
+
+class DashApiFilterTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        level = 'sector'
+        year = '2015'
+        cls.econ = m.Leontief(level, year, sql=True)
+        cls.response = client.get(f'/dash/{level}/{year}')
+        cls.parsed = cls.response.json()
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.econ
+        del cls.response
+        del cls.parsed
+
+    def test_dash_filter_resp(self):
+        response = client.get('/dash/sector/2015/')
+        parsed = response.json()
+        self.assertEqual(self.parsed['level'], level)
+        self.assertEqual(self.parsed['year'], year)

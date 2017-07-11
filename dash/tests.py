3453a7e4fa7886200ad6acb6a107a9a0834ca0c5
Hayden Turek <haydenturek@gmail.com>
2017-07-10 20:48:15 -0500

    passed tests for dash index view

--- dash/tests.py
+++ dash/tests.py
@@ -7,7 +7,5 @@ class DashApiTest(TestCase):
     def test_dash_index_resp_sector_mostrec_year(self):
         response = client.get('/dash/')
         parsed = response.json()
-        self.assertEqual(parsed.level, 'sector')
-        self.assertEqual(parsed.year, '2015')
-
-# Create your tests here.
+        self.assertEqual(parsed['level'], 'sector')
+        self.assertEqual(parsed['year'], 2015)
--- dash/views.py
+++ dash/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+
+def index(request):
+    res = {'level': 'sector', 'year': 2015}
+    return HttpResponse(json.dumps(res), content_type='application/json')

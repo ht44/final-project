d4334ed652d21ac605843f4528b6e021f1761e08
Hayden Turek <haydenturek@gmail.com>
2017-07-12 17:37:07 -0500

    passed tests for api param get request

--- dash/views.py
+++ dash/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from iomodel import model as m
+import decimal
 import json
+
+deci = decimal.Decimal
+decimal.getcontext().prec = 1
 
 def index(request):
     econ = m.Leontief('sector', '2015', sql=True)
@@ -26,6 +30,7 @@ def index(request):
     return HttpResponse(json.dumps(resp), content_type='application/json')
 
 def filter(request, level, year):
+    print(request.GET)
     econ = m.Leontief(level, year, sql=True)
     econ.balance()
     resp = {
@@ -45,4 +50,14 @@ def filter(request, level, year):
         'unit_requirements': econ.unit_requirements.tolist(),
         'unit_price': econ.unit_price.tolist(),
         }
+
+    if request.GET.get('tax', False) == 'true':
+        x = deci(request.GET.get('0'))
+        y = deci(request.GET.get('1'))
+        z = deci(request.GET.get('2'))
+
+        econ.model_price([(0, x), (1, y), (2, z)])
+
+        resp['rel_unit_price'] = econ.rel_unit_price.tolist()
+
     return HttpResponse(json.dumps(resp), content_type='application/json')
